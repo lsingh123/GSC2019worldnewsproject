58f6850fbc3fa6a6f23340fea903cd9d649056cc
Lavanya Singh <lavanyasingh@Lavanyas-MacBook-Air-011219.local>
2019-08-01 15:31:16 -0700

    fixed get_meta.py using kenji's tools

--- get_meta.py
+++ get_meta.py
@@ -1,134 +1,117 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jul 29 13:10:42 2019
+Created on Thu Aug  1 10:19:18 2019
 
 @author: lavanyasingh
 """
 
-from bs4 import BeautifulSoup
+from requests_html import HTMLSession
+from lxml import etree
+from io import StringIO
+from urllib import parse
+import concurrent.futures
 import csv
 import time
-import os
-os.chdir(os.path.dirname(os.getcwd()))
-from pyppeteer import launch
-import asyncio
-import logging
-import sys
 
-# THIS SCRIPT IS BROKEN, KENJI IS WORKING ON USING HIS EXISTING HEADLESS
-# BROWSER CLUSTER TO GET FULL DOM
+# Scrape metadata from Kenji's service that returns rendered HTML
 
-logger = logging.getLogger('__worldnews__')
-logging.basicConfig(filename='logfle.log',filemode = 'w')
-
-def my_handler(type, value, tb):
-    logger.exception("Uncaught exception: {0}".format(str(value)))
-
-# Install exception handler
-sys.excepthook = my_handler
-
-class FBOGCrawler():
-
-    PATH = os.getcwd() + "/data/raw"
-
-    def __init__(self):
-        self.res, self.urls = [], []
+class MetadataParser():
+    
+    def __init__(self, infile = "data/all_raw_cleaned.csv", 
+                 outfile = "data/metadata.csv"):
+        self.infile, self.outfile = infile, outfile
+        self.session = HTMLSession()
+        self.parser = etree.HTMLParser()
+        self.urls = []
         self.read_in()
-
+    
+    # read in cleaned URLs from CSV
     def read_in(self):
-        with open(self.PATH + "/all_raw_cleaned.csv", 'r') as f:
+        with open(self.infile, 'r') as f:
             reader = csv.reader(f, delimiter=',')
             for line in reader:
-                if len(self.urls) > 50: break
+                #if len(self.urls) > 100: break
                 self.urls.append("http://" + "".join(line[1]))
         print("DONE READING")
     
-    async def get_browser(self):
-        return await launch({"headless": True, "args": ['--nosandbox']})
+    # load the HTML tree for a given URL
+    def load_tree(self, url):
+        url = parse.quote(url)
+        r = self.session.get("http://crawl-services.us.archive.org:8200/web?url={url}&format=html".format(url=url))
+        html = r.html.html
+        return etree.parse(StringIO(html), self.parser)
     
-    async def get_page(self, browser, url):
+    # get open graph data for a given attribute (title, locale, description)
+    def get_og(self, tree, attribute):
         try:
-            page = await browser.newPage()
-            await page.goto(url, timeout = 100000)
-            await page.waitFor("head")
-            c = await page.content()
-            await page.close()
-            return c
-        except Exception: 
-            raise
-    
-    async def parse_html(self, browser, url):
-        try:
-            html = await self.get_page(browser, url)
-            soup = BeautifulSoup(html, features="html.parser")
-            head = soup.head
-            title = self.get_attr(head, "title")
-            desc = self.get_attr(head, "description")
-            locale = self.get_locale(head)
-            return [url, title, desc, locale]
-        except TimeoutError:
-            raise
-        except Exception as e:
-            raise
-            #logger.exception(traceback.format_exc())
-            return [url, str(e)]
-
-    def get_attr(self, head, attr):
-        try:
-            return head.find(attrs={"property": "og:" + attr})['content']
-        except TypeError:
+            query = "//meta[@property='og:{a}']/@content".format(a=attribute)
+            return tree.xpath(query)[0]
+        except IndexError:
             pass
         try:
-            return head.find(attrs={"property": "twitter:" + attr})['content']
-        except TypeError:
+            query = "//meta[@name='{a}']/@content".format(a=attribute)
+            return tree.xpath(query)[0]
+        except IndexError:
             pass
         try:
-            return head.find("title").text
-        except AttributeError:
-            return ""
-
-    def get_locale(self, head):
-        try:
-            return head.find(attrs={"property": "og:locale"})['content']
-        except TypeError:
-            return ""
-        
-    async def parse_all(self):
-        browser = await self.get_browser()
-        for url in self.urls:
+            query = "//meta[@name='twitter:{a}']/@value".format(a=attribute)
+            return tree.xpath(query)[0]
+        except IndexError:
+            return None
+    
+    def get_title(self, tree):
+        title = self.get_og(tree, 'title')
+        if title is None:
             try:
-                self.res.append(await self.parse_html(browser, url))
-                print(str(len(self.res)),end="\r")
-            except Exception as e:
-                print(url, e)
-                browser = await self.get_browser()
-        await browser.close()
-    
-    def callback(self, fut):
+                title =  tree.xpath("//title")[0].text
+            except IndexError:
+                title = None
+        return title
+        
+    def get_description(self, tree):
+        desc = self.get_og(tree, 'decription')
+        if desc is None:
+            try:
+                desc =  tree.xpath("//meta[@name='description']/@content")[0]
+            except IndexError:
+                desc = None
+        return desc
+
+    # scrape and parse a given URL
+    def parse_url(self, url):
         try:
-            fut.result()
+            tree = self.load_tree(url)
+            title = self.get_title(tree)
+            desc = self.get_description(tree)
+            locale = self.get_og(tree, "locale")
+            results = [url, title, desc, locale]
+            # replace none values with the empty string
+            return ["" if res is None else res for res in results]
         except Exception as e:
             print(e)
-            raise
-        
-    def write_meta(self):
-        with open(self.PATH + "/meta_good2.csv", 'w') as outf:
+            print(url)
+            return [url]
+            
+    
+    def write_meta(self, results):
+        with open(self.outfile, 'w') as outf:
             w = csv.writer(outf, delimiter=',', quotechar='"',
                            quoting=csv.QUOTE_MINIMAL)
-            for url in self.res:
+            for url in results:
                 w.writerow(url)
         print("WROTE ALL METADATA")
-
+    
     def main(self):
-        loop = asyncio.get_event_loop()
         time1 = time.time()
-        loop.run_until_complete(self.parse_all())
+        with concurrent.futures.ThreadPoolExecutor(max_workers=16) as pool:
+            results = pool.map(self.parse_url, self.urls)
         time2 = time.time()
-        print(f"Took {time2-time1:.2f} s")
-        self.write_meta()
+        self.write_meta(results)
+        print(f"Took {time2-time1:.2f} seconds")
+        self.session.close()
 
-if __name__ == "__main__":
-    crawler = FBOGCrawler()
-    crawler.main()
-    print("FINISHED RUNNING")
+if __name__ == '__main__':
+    parser = MetadataParser()
+    parser.main()
+        
