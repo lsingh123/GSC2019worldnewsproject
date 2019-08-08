773734a954d86e094addd63e7d21e5414e8fcced
Lavanya Singh <lavanyasingh@Lavanyas-MacBook-Air-011219.local>
2019-08-08 12:12:41 -0700

    debugging

--- clean_brokenlinks.py
+++ clean_brokenlinks.py
@@ -5,9 +5,38 @@ Created on Tue Aug  6 12:45:23 2019
 
 @author: lavanyasingh
 """
+# script to filter out broken links
 
 import csv
 
-def clean():
-    with open("data/codes2.csv", 'r') as f:
-        reader = csv.reader(f, delimiter=',')
+def get_codes():
+    codes = {}
+    with open("data/codes2.csv", 'r') as inf:
+        reader = csv.reader(inf, delimiter=',')
+        for line in reader:
+            if line[1] != "ERROR":
+                code = int(line[1])
+            try:
+                redirect = line[2]
+            except IndexError:
+                redirect = None
+            codes.update({line[0]:[code, redirect]})
+    return codes
+
+def write_codes():
+    codes = get_codes()
+    count = 0
+    with open("data/all_raw_cleaned.csv", 'r') as inf, \
+        open("data/all_working.csv", 'w') as outf:
+        reader = csv.reader(inf, delimiter=',')
+        w = csv.writer(outf, delimiter= ',', quotechar = '"', 
+                           quoting = csv.QUOTE_MINIMAL)
+        for line in reader:
+            code = codes['http://' + line[1]]
+            if int(code[0]) < 400:
+                count += 1
+                line[11] = code[1]
+                w.writerow(line)
+    print(count)
+                
+write_codes()
--- tableau_prep.py
+++ tableau_prep.py
@@ -7,36 +7,32 @@ Created on Wed Jul 31 09:56:27 2019
 """
 
 import csv
-import os
 from fuzzywuzzy import process
 import helpers
+import argparse
 
 class mapDataMaker():
     
-    def __init__(self, path = os.path.dirname(os.getcwd()), 
-                 countryfile = "/data/countries.csv", 
-                 infile = "/data/all_raw_cleaned1.csv",
-                 outfile = "/data/geodata.csv"):
-        self.countryfile, self.path = countryfile, path
-        self.infile, self.outfile = self.path + infile, self.path + outfile
+    def __init__(self, countryfile = "data/countries.csv", 
+                 infile = "data/all_raw_cleaned1.csv",
+                 outfile = "data/geodata.csv"):
+        self.countryfile = countryfile
+        self.infile, self.outfile = infile, outfile
         self.countries = self.read_countries()
     
     # returns a dictionary {country name: [iso, latitude, longitude, name]}
     def read_countries(self):
         countries = {}
-        with open(self.path + self.countryfile, 'r') as inf:
+        with open(self.countryfile, 'r') as inf:
             reader = csv.reader(inf, delimiter=',')
             next(reader)
-
             for line in reader:
                 countries.update({line[3]: line})
         return countries
     
     def process(self):
         total = 0
-
         with open(self.infile, 'r') as inf, open(self.outfile, 'w') as outf:
-
             reader = csv.reader(inf, delimiter=',')
             w = csv.writer(outf, delimiter= ',', quotechar = '"', 
                                quoting = csv.QUOTE_MINIMAL)
@@ -52,10 +48,23 @@ class mapDataMaker():
                 print(str(total),end="\r")
                 row = self.countries[country] + [line[1]]
                 w.writerow(row)
-                    
+
+# create argument parser
+def create_parser():
+    argp = argparse.ArgumentParser(
+            description='Prep country data for Tableau')
+    argp.add_argument('-inf', '--infile', nargs='?',
+                      default='data/all_working.csv', type=str,
+                      help='csv file to read URLs in from')
+    argp.add_argument('-outf', '--outfile', nargs='?',
+                      default='data/geodata2.csv', type=str,
+                      help='csv file to write metadata to')
+    return argp
 
 if __name__ == '__main__':
-    mapMaker = mapDataMaker()
+    argp = create_parser()
+    args = argp.parse_args()
+    mapMaker = mapDataMaker(infile=args.infile, outfile=args.outfile)
     mapMaker.process()        
         
     
